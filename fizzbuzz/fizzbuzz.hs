9b04652449dbf2bc4683a22e9d451b8f7ee74871
Chris Marshall <chris@chrismar035.com>
2014-01-29 11:35:50 -0500

    Not clean, but working Haskell fizzbuzz

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -1,10 +1,13 @@
 main = do
-  numbers <- take 100 [1,2..]
-  map (fizzbuzz) numbers
+  numbers <- return $ take 100 [1,2..]
+  mapM_ putStrLn $ map fizzbuzz numbers
 
-fizzbuzz number = if (number % 5 && number % 3) then
-                    putStrLn "FizzBuzz"
-                  else if (number % 5) then
-                    putStrLn "Buzz"
-                  else if (number % 3) then
-                    putStrLn "Fizz"
+fizzbuzz :: Integer -> String
+fizzbuzz number = if (((mod number 5) == 0) && ((mod number 3) == 0)) then
+                    "FizzBuzz"
+                  else if ((mod number 5) == 0) then
+                    "Buzz"
+                  else if ((mod number 3) == 0) then
+                    "Fizz"
+                  else
+                    show number
