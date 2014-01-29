0297ef3409ca665eac35013ba7e8a976dea2d07b
Chris Marshall <chris@chrismar035.com>
2014-01-29 12:43:33 -0500

    Use suggestions from FP Complete

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -1,10 +1,10 @@
 main = do
-  numbers <- return $ take 100 [1,2..]
-  mapM_ putStrLn $ map fizzbuzz numbers
+  let numbers = take 100 [1,2..]
+  mapM_ (putStrLn . fizzbuzz) numbers
 
 fizzbuzz :: Integer -> String
-fizzbuzz number | (number `by` 15) = "FizzBuzz"
-                | (number `by` 5) = "Buzz"
-                | (number `by` 3) = "Fizz"
+fizzbuzz number | number `by` 15 = "FizzBuzz"
+                | number `by` 5 = "Buzz"
+                | number `by` 3 = "Fizz"
                 | otherwise = show number
                 where by x y = x `rem` y == 0
